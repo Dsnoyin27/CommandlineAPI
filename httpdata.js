62e8df34fd3b99848e6f3fb09ffd69601a22cc40
Adesina <dsnoyin27@gmail.com>
2017-06-07 16:49:06 +0100

    The javascrpt file and JSON files.
    I used the javascript code the get weather information from Dark Sky library

---
+++ httpdata.js
@@ -0,0 +1,26 @@
+// This code requires the module 
+var Forecast = require('forecast');
+ 
+// This code initializes the module
+var forecast = new Forecast({
+  service: 'darksky',
+  key: '273426f477691b3057a726fc2c9f72ee',
+  units: 'celcius',
+  cache: true,      // Cache API requests 
+  ttl: {            // How long to cache requests. Uses syntax from moment.js: http://momentjs.com/docs/#/durations/creating/ 
+    minutes: 27,
+    seconds: 45
+  }
+});
+ 
+// This code retrieves weather information from coordinates in lagos
+forecast.get([6.465422, 3.406448], function(err, weather) {
+  if(err) return console.dir(err);
+  console.dir(weather);
+});
+ 
+// This code retrieves weather information, ignoring the cache 
+forecast.get([6.465422, 3.406448], true, function(err, weather) {
+  if(err) return console.dir(err);
+  console.dir(weather);
+});
